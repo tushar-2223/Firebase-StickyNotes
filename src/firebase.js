4b9176be0d10269c8dc9a6e2a23af3bf856101a7
Tushar Pankhaniya <87109400+tusharpankhaniya@users.noreply.github.com>
2023-01-29 16:21:27 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,11 +2,11 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCCEUmUN0vNnzG3Jpz_n25P85YnirvDqos",
-  authDomain: "fir-sticky-notes.firebaseapp.com",
-  databaseURL: "https://fir-sticky-notes-default-rtdb.firebaseio.com",
+  apiKey: < APIKEY >,
+  authDomain: <>,
+  databaseURL: <FIREBASEURL>,
   projectId: "fir-sticky-notes",
-  storageBucket: "fir-sticky-notes.appspot.com",
+  storageBucket: <>,
   messagingSenderId: "907951786814",
   appId: "1:907951786814:web:fe936b3bf64f5456623a6f",
   measurementId: "G-M3XXKKH8NT"
